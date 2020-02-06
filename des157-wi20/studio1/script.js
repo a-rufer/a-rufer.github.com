5b7db6ec6cb18536dd64a2884a2b887a07b29768
Alana Rufer <alana.rufer@gmail.com>
2020-02-06 15:59:51 -0800

    linked studio1 to portal page and made minor edits

--- des157-wi20/studio1/script.js
+++ des157-wi20/studio1/script.js
@@ -1,23 +1,28 @@
 
 
-
 (function(){
 
 
     "use strict"
 
-    const num_blanks = 14;
-    var blanks = [];
+    const num_blanks = 14; 
+    let blanks = [];
 
+    // action to take upon submission
     document.addEventListener("submit", function(event){
+
         event.preventDefault();
+
+        // put all of the inputs in the blanks array
         for(let i = 0; i < num_blanks; i++) {
             blanks[i] = document.getElementById(`blank${i+1}`).value;
         }
+
         fillInBlanks(blanks);
         revealStory();
     });
 
+    // fills in the blanks of the story with the words from the blanks array
     function fillInBlanks(blanks) {
         for(let i = 0; i < blanks.length; i++) {
             let blank = document.getElementById(`${i+1}`);
@@ -25,12 +30,10 @@
         }
     }
 
+    // toggles visibility on the story and the instructions
     function revealStory() {
         document.getElementById("story").removeAttribute("class"); // show story
-        document.getElementById("grade").style.visibility = "visible" // show grade
         document.getElementById("instructions").setAttribute("class", "hidden"); // hide instructions
-
-
     }
 
 
