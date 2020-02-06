48bca4827db0df06eda0ba88bd1bb837b627ec62
Alana Rufer <alana.rufer@gmail.com>
2020-02-06 14:52:32 -0800

    worked on studio 1

--- des157-wi20/studio1/script.js
+++ des157-wi20/studio1/script.js
@@ -1,21 +1,42 @@
 
 
-"use strict"
 
-const num_blanks = 14;
-var blanks = [];
+(function(){
 
-document.addEventListener("submit", function(event){
-    event.preventDefault();
-    for(let i = 0; i < num_blanks; i++) {
-        blanks[i] = document.getElementById(`blank${i+1}`).value;
+
+    "use strict"
+
+    const num_blanks = 14;
+    var blanks = [];
+
+    document.addEventListener("submit", function(event){
+        event.preventDefault();
+        for(let i = 0; i < num_blanks; i++) {
+            blanks[i] = document.getElementById(`blank${i+1}`).value;
+        }
+        fillInBlanks(blanks);
+        revealStory();
+    });
+
+    function fillInBlanks(blanks) {
+        for(let i = 0; i < blanks.length; i++) {
+            let blank = document.getElementById(`${i+1}`);
+            blank.innerHTML = blanks[i];
+        }
     }
-    fillInBlanks(blanks);
-});
 
-function fillInBlanks(blanks) {
-    for(let i = 0; i < blanks.length; i++) {
-        let blank = document.getElementById(`${i+1}`);
-        blank.innerHTML = blanks[i];
+    function revealStory() {
+        document.getElementById("story").removeAttribute("class");
+        document.getElementById("grade").removeAttribute("class");
+        document.getElementById("grade").style.display = "initial"
+        document.getElementById("instructions").setAttribute("class", "hidden");
+        //document.querySelector("#story").style.background = "url(B-.gif) no-repeat";
+        //document.querySelector("#story").style.backgroundSize = "contain";
+        //document.querySelector("#story").style.backgroundPosition = "right";
+
+
+
     }
-}
+
+
+}());
